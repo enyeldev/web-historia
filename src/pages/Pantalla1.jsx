a7b5b4f8a7e521efbcd203fb5fd41e7cad082597
Enyel <medranogarciaenyel@gmail.com>
2023-12-10 16:22:04 -0400

    segundo-commit-nose-que-hice

--- src/pages/Pantalla1.jsx
+++ src/pages/Pantalla1.jsx
@@ -6,12 +6,12 @@ import { Btn } from '../components/Btn'
 export const Pantalla1 = () => {
     return (
         <>
-            <div className="w-full h-[460px] flex justify-between overflow-hidden ">
-                <div className=" w-full md:w-[50%] max-w-[1000px] p-4 flex flex-col justify-center items-center md:items-start relative gap-4">
+            <div className="w-full h-[560px] flex justify-between overflow-hidden ">
+                <div className=" w-full md:w-[50%] max-w-[1000px] px-4 flex flex-col justify-center items-center md:items-start relative gap-4">
                     <h1 className='text-black font-black  text-4xl md:text-5xl text-center md:text-start'>La <sapn className="text-green-400">Contaminacion </sapn> <br className='hidden' />Ambiental</h1>
                     <p className='w-[90%] text-center text-sm md:text-left'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas nulla dolore nobis velit illum.</p>
                     <Btn ruta={'contaminacion-ambiental'} />
-                    <img src={blob} alt="Blob" className='absolute w-[150px] top-[-1%] left-[80%] z-10 hidden md:block' />
+                    <img src={blob} alt="Blob" className='absolute w-[150px] top-[-1%] left-[90%] z-10 hidden md:block' />
                 </div>
 
                 <div className="w-[50%] h-full bg-center bg-cover bg-no-repeat hidden md:block" style={{ backgroundImage: `url(${imgHero})` }}></div>
